1359d366a4623531de266da92316ee8abced0c6f
minasyans777 <minasyans777@gmail.com>
2025-12-27 00:55:44 +0400

    Update build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
 
-read -p "Please write the full path of your program. " program_path
+FILE=""
+OUTPUT=""
 
-go build -o /home/sam/pingpong $program_path 
+while getopts "s:o:h" opt; do
+	case $opt in
+		s)
+			FILE=$OPTARG
+			;;
+		o)
+			OUTPUT=$OPTARG
+			;;
+		h)
+          		echo "Options:"
+            		echo "  -s    Specify the full path of the program to build"
+            		echo "  -o    Specify the directory where to save the built program"
+            		echo "  -h    Show this help message"
+            		exit 0
+            		;;
+		*)
+			echo "Invalid option!"
+      			echo "Usage: $0 -s full_path_to_program -o output_directory [-h]"
+      			exit 1
+      			;;
+	esac
+done
+
+if [[ -z $FILE ]]; then
+	echo "Error: -s option is required"
+	echo "Usage: $0 -s full_path_to_program -o output_directory [-h]"
+	exit 1
+fi
+
+if [[ -z $OUTPUT ]]; then
+	OUTPUT="."
+fi
+
+go build -o "$OUTPUT" "$FILE"
