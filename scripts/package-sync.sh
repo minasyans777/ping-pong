e988510614f4fef10bd533b771f15a6662996622
minasyans777 <minasyans777@gmail.com>
2025-12-29 19:51:56 +0400

    Update package-sync.sh

--- scripts/package-sync.sh
+++ scripts/package-sync.sh
@@ -2,7 +2,7 @@
 
 set -e 
 
-OPTIONS=$(getopt -o r:s:d:m:v:R:k:I:h --long arch:,help -- "$@")
+OPTIONS=$(getopt -o r:s:d:m:v:R:k:I:D:h --long arch:,help -- "$@")
 eval set -- "$OPTIONS"
 
 REPO=""
@@ -12,6 +12,7 @@ PATH_REPO_PACKAGE=""
 PATH_METADATA_PACKAGE=""
 VERSION=""
 PATH_RELEASE=""
+PATH_RELEASE_DIR=""
 GPG_KEY_ID=""
 PATH_INRELEASE=""
 
@@ -45,6 +46,10 @@ while true;do
 			PATH_RELEASE=$2
 			shift 2
 			;;
+		-D)
+			PATH_RELEASE_DIR=$2
+			shift 2
+			;;
 		-k)
 			GPG_KEY_ID=$2
 			shift 2
@@ -61,7 +66,8 @@ while true;do
 			echo " -d                Specify the full paths of the packages inside the repository"
 			echo " -m                Specify where the Packages file should be generated"
 			echo " -v                Specify the package version"
-			echo " -R                Specify where the Release file should be generated"
+			echo " -D                Specify directory where the Release file should be generated"
+			echo " -R                Specify where the Release file is located "
 			echo " -k                Specify the GPG key ID"
 			echo " -I                Specify where the InRelease file should be generated"
                         echo " -h|--help         Show this help message"
@@ -138,43 +144,21 @@ if [[ "$PATH_NEW_PACKAGE" == *.deb ]];then
 	sudo dpkg-scanpackages --arch "$ARCH" pool/ | sudo tee "$PATH_METADATA_PACKAGE"
 
 	cat "$PATH_METADATA_PACKAGE" | gzip -9 |sudo tee "$PATH_METADATA_PACKAGE.gz"
-	echo '#!/bin/sh
-	set -e
 
-	do_hash() {
-	    HASH_NAME=$1
-	    HASH_CMD=$2
-	    echo "${HASH_NAME}:"
-	    for f in $(find -type f); do
-	        f=$(echo $f | cut -c3-) # remove ./ prefix
-	        if [ "$f" = "Release" ]; then
-	            continue
-	        fi
-	        echo " $(${HASH_CMD} ${f}  | cut -d" " -f1) $(wc -c $f)"
-	    done
-	}' >> ~/generate-release.sh
-
-	cat << EOF >> ~/generate-release.sh
-	cat << EOT
-	Origin: Example Repository
-	Label: Example
-	Suite: stable
-	Codename: stable
-	Version: $VERSION
-	Architectures: $ARCH
-	Components: main
-	Description: Game
-	Date: $(date -Ru)
-EOT
+	cat << EOF | sudo tee  "$PATH_RELEASE"
+Origin: Example Repository
+Label: Example
+Suite: stable
+Codename: stable
+Version: $VERSION
+Architectures: $ARCH
+Components: main
+Description: Game
+Date: $(date -Ru)
 EOF
-	echo '
-	do_hash "MD5Sum" "md5sum"
-	do_hash "SHA1" "sha1sum"
-	do_hash "SHA256" "sha256sum" 
-	'>> ~/generate-release.sh && chmod +x ~/generate-release.sh
 
-	~/generate-release.sh | sudo tee "$PATH_RELEASE"
-	
+	apt-ftparchive release "$PATH_RELEASE_DIR" | sudo tee -a "$PATH_RELEASE"
+
 	cat "$PATH_RELEASE" | gpg --default-key "$GPG_KEY_ID" -abs | sudo tee "$PATH_RELEASE.gpg"
 
 	cat "$PATH_RELEASE" | gpg --default-key "$GPG_KEY_ID" -abs --clearsign | sudo tee "$PATH_INRELEASE"
@@ -195,6 +179,17 @@ if [[ "$PATH_NEW_PACKAGE" == *.rpm  ]];then
 
 	gpg --detach-sign --armor repodata/repomd.xml
 fi
+
+
+
+
+
+
+
+
+
+
+
 
 
 
